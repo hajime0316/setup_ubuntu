e340125a0b297fbc0c759cb552bf4f76a569710c
Hajime <hajime0316@s.okayama-u.ac.jp>
2020-04-27 21:37:10 +0900

    Revert "Add install_google_chrome.bash"
    This reverts commit 3b8ec5373515c0e6d4ec066ea99b0c1717161e21.

--- install_google_chrome.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-sudo apt update
-curl https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
-sudo apt update
-sudo apt install -y google-chrome-stable
