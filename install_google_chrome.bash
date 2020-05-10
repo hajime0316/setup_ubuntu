48f1730422ae67a415b1b43825eede40f2350522
Hajime <hajime0316@s.okayama-u.ac.jp>
2020-05-10 15:44:26 +0900

    Add sudo_knowing_password function

--- install_google_chrome.bash
+++ install_google_chrome.bash
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+sudo_knowing_password() {
+    if ! ${password+:} false; then
+        printf "[sudo] $USER のパスワード: "
+        read -s password
+    fi
+
+    echo "$password" | sudo -S $*
+}
+
 set -e
 
 sudo apt update
