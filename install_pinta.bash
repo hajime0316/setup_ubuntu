79cf0d75c6813d9874387e510878ba07139901c3
Hajime <hajime0316@s.okayama-u.ac.jp>
2020-06-07 02:13:55 +0900

    Add install_pinta.bash

---
+++ install_pinta.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+# 関数定義
+sudo_knowing_password() {
+    if ! ${password+:} false; then
+        printf "[sudo] $USER のパスワード: "
+        read -s password
+    fi
+
+    echo "$password" | sudo -S $*
+}
+
+sudo_knowing_password apt update
+sudo_knowing_password apt install -y pinta
