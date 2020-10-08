faf61160ab8ff73d21b1813ef575a3e11573c0c1
Hajime <hajime0316@s.okayama-u.ac.jp>
2020-10-09 02:22:12 +0900

    Fix install command in install_atom.bash

--- install_atom.bash
+++ install_atom.bash
@@ -11,8 +11,6 @@ sudo_knowing_password() {
 
 set -e
 
-sudo_knowing_password apt update
-wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
-sudo_knowing_password sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+sudo_knowing_password add-apt-repository ppa:webupd8team/atom
 sudo_knowing_password apt update
 sudo_knowing_password apt install -y atom
