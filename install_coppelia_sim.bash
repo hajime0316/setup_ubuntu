951a8c0df9fc14703b14ce82ea7c0e9aa7eb1fd0
hajime0316 <hajime0316@s.okayama-u.ac.jp>
2020-12-03 23:23:29 +0900

    Uncomment wget calling in install_coppelia_sim.bash

--- install_coppelia_sim.bash
+++ install_coppelia_sim.bash
@@ -7,9 +7,9 @@ set -e
 FILE_NAME_WITHOUT_EXTENSION="CoppeliaSim_Edu_V4_1_0_Ubuntu18_04"
 LOCAL_COPPELIA_SIM_DIR="$HOME/CoppeliaSim"
 
-# wget "https://coppeliarobotics.com/files/${FILE_NAME_WITHOUT_EXTENSION}.tar.xz"
+wget "https://coppeliarobotics.com/files/${FILE_NAME_WITHOUT_EXTENSION}.tar.xz"
 
-# tar Jxvf "$FILE_NAME_WITHOUT_EXTENSION.tar.xz"
+tar Jxvf "$FILE_NAME_WITHOUT_EXTENSION.tar.xz"
 mv "$FILE_NAME_WITHOUT_EXTENSION" "$LOCAL_COPPELIA_SIM_DIR"
 rm "$FILE_NAME_WITHOUT_EXTENSION.tar.xz"
 
@@ -17,3 +17,5 @@ rm "$FILE_NAME_WITHOUT_EXTENSION.tar.xz"
 echo "" >>~/.bashrc
 echo "# CoppeliaSimを起動するエイリアス" >>~/.bashrc
 echo "alias copsim='${LOCAL_COPPELIA_SIM_DIR}/coppeliaSim.sh'" >>~/.bashrc
+
+# TODO: B0 Remote APIをゆうこうにする
