692223cb2592267e6b057a8491a83b3a35ee4b5c
hajime0316 <hajime0316@s.okayama-u.ac.jp>
2020-12-05 02:34:12 +0900

    Install curl in install_coppelia_sim.bash

--- install_coppelia_sim.bash
+++ install_coppelia_sim.bash
@@ -3,6 +3,19 @@
 # -eオプションを有効
 set -e
 
+# 関数定義
+sudo_knowing_password() {
+    if ! ${password+:} false; then
+        printf "[sudo] $USER のパスワード: "
+        read -s password
+    fi
+
+    echo "$password" | sudo -S $*
+}
+
+sudo_knowing_password apt update
+sudo_knowing_password apt install -y curl
+
 # V-REP本体のインストール
 FILE_NAME_WITHOUT_EXTENSION="CoppeliaSim_Edu_V4_1_0_Ubuntu18_04"
 LOCAL_COPPELIA_SIM_DIR="$HOME/CoppeliaSim"
