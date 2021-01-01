53cba40f51e946563582291459395db53bc4d6d3
hajime0316 <hajime0316@s.okayama-u.ac.jp>
2021-01-02 00:59:10 +0900

    Add TODO comment

--- install_fonts.bash
+++ install_fonts.bash
@@ -12,5 +12,8 @@ sudo_knowing_password() {
 set -e
 
 sudo_knowing_password apt update
+# TODO: ricty-diminishedは以下のGitHubから取得するようにする
+# https://github.com/edihbrandon/RictyDiminished
+# 理由：aptからインストールしたricty-diminishedはバッククオートの表示に不具合がある
 sudo_knowing_password apt install -y fonts-ricty-diminished
 sudo_knowing_password apt install -y fonts-migmix
