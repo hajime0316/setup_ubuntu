808692f2ddb562659fb13641739089ffc67dbdcf
Hajime <hajime0316@s.okayama-u.ac.jp>
2021-01-10 15:28:48 +0900

    Add comment in install_atom.bash

--- install_atom.bash
+++ install_atom.bash
@@ -17,3 +17,5 @@ wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
 sudo_knowing_password sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
 sudo_knowing_password apt update
 sudo_knowing_password apt install atom -y
+
+# 参考URL: https://flight-manual.atom.io/getting-started/sections/installing-atom/#platform-linux
