3ccf09f093c3d35fba3bd12b0d9834f975e04bc0
hajime0316 <hajime0316@s.okayama-u.ac.jp>
2021-06-25 23:39:59 +0900

    Install VS Code via snap

--- install_vscode.bash
+++ install_vscode.bash
@@ -12,10 +12,4 @@ sudo_knowing_password() {
     echo "$password" | sudo -S "$@"
 }
 
-# VS Codeのインストール
-# 参考にしたサイト: https://qiita.com/yoshiyasu1111/items/e21a77ed68b52cb5f7c8
-sudo_knowing_password apt update
-sudo_knowing_password apt install -y curl
-curl -L https://go.microsoft.com/fwlink/?LinkID=760868 -o vscode.deb
-sudo_knowing_password apt install ./vscode.deb
-rm vscode.deb
+sudo_knowing_password snap install code --classic
