cd3e3038ef6f97b359a34a8102187eb28ba02c2c
hajime0316 <hajime0316@s.okayama-u.ac.jp>
2021-09-26 23:26:52 +0900

    atcoderのインストーラに関数定義を追加

--- install_atcoder_tools.bash
+++ install_atcoder_tools.bash
@@ -1,4 +1,21 @@
 #!/bin/bash
 
-sudo apt install python3-pip
+set -e
+
+sudo_knowing_password() {
+    if ! ${password+:} false; then
+        printf "[sudo] $USER のパスワード: "
+        read -s password
+    fi
+
+    echo "$password" | sudo -S "$@"
+}
+
+SCRIPT_DIR=$(
+    cd $(dirname $0)
+    pwd
+)
+
+sudo_knowing_password apt update
+sudo_knowing_password apt install python3-pip -y
 pip3 install atcoder-tools
