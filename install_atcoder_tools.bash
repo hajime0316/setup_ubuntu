5a1790df64f370d0ceb5a3216492cf30aae9cdd3
hajime0316 <hajime0316@s.okayama-u.ac.jp>
2021-09-26 23:38:14 +0900

    atcoder toolsの設定ファイルを移動するコマンドの追加

--- install_atcoder_tools.bash
+++ install_atcoder_tools.bash
@@ -19,3 +19,14 @@ SCRIPT_DIR=$(
 sudo_knowing_password apt update
 sudo_knowing_password apt install python3-pip -y
 pip3 install atcoder-tools
+
+# atcoder-workspaceフォルダを作成
+ATCODER_WORKSPACE_DIR=~/atcoder-workspace
+mkdir -p "${ATCODER_WORKSPACE_DIR}"
+mkdir -p "${ATCODER_WORKSPACE_DIR}/.vscode"
+
+# 設定ファイルを移動
+cp "${SCRIPT_DIR}/config_files/atcodertools/.atcodertools.toml" ~
+cp "${SCRIPT_DIR}/config_files/atcodertools/my_template.cpp" "${ATCODER_WORKSPACE_DIR}"
+cp -r "${SCRIPT_DIR}/config_files/atcodertools/ac-library" "${ATCODER_WORKSPACE_DIR}"
+cp "${SCRIPT_DIR}/config_files/atcodertools/*.json" "${ATCODER_WORKSPACE_DIR}/.vscode"
