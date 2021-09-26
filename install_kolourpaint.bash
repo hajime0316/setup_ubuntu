25a011b942a4c45ad8f986c53fb70ee715183ff1
hajime0316 <hajime0316@s.okayama-u.ac.jp>
2021-09-26 23:39:53 +0900

    sudoコマンドをsudo_knowing_passwordに変更

--- install_kolourpaint.bash
+++ install_kolourpaint.bash
@@ -12,4 +12,4 @@ sudo_knowing_password() {
     echo "$password" | sudo -S "$@"
 }
 
-sudo snap install kolourpaint
+sudo_knowing_password snap install kolourpaint
--- install_vcstool.bash
+++ install_vcstool.bash
@@ -17,7 +17,7 @@ sudo_knowing_password() {
 
 sudo_knowing_password apt update
 sudo_knowing_password apt install -y python-pip
-sudo -H pip install vcstool
+sudo_knowing_password -H pip install vcstool
 
 echo '# vcstoolの入力補完を有効化' >> ~/.bashrc
 echo 'source /usr/local/share/vcstool-completion/vcs.bash' >> ~/.bashrc
