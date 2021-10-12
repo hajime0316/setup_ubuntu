e4b2f2c05b69956c66923e3fbc133b4dc77a3c8c
Hajime <hajime0316@s.okayama-u.ac.jp>
2021-10-12 11:46:11 +0900

    install_git_ex_command.bashの編集

---
+++ install_git_ex_command.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+sudo_knowing_password() {
+    if ! ${password+:} false; then
+        printf "[sudo] $USER のパスワード: "
+        read -s password
+    fi
+
+    echo "$password" | sudo -S "$@"
+}
+
+SCRIPT_DIR=$(
+    cd $(dirname $0)
+    pwd
+)
+
+USER_BIN_DIR = ~/bin
+
+mkdir -p ~/bin
+cp "${SCRIPT_DIR}/config_files/git_ex_command/*" "${USER_BIN_DIR}"
